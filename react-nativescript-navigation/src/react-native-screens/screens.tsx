da75acf7f11bcd4a102287a3a61fe7fede1e54b5
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-06-07 20:44:13 +0100

    Provisional implementation of Screens

---
+++ react-nativescript-navigation/src/react-native-screens/screens.tsx
@@ -0,0 +1,198 @@
+import React from 'react';
+import { ViewBaseAttributes } from "react-nativescript/dist/shared/NativeScriptJSXTypings";
+import { FlexboxLayoutAttributes } from "react-nativescript/dist/lib/react-nativescript-jsx";
+type RNSStyle = ViewBaseAttributes["style"];
+type StyleProp<T> = T;
+type TextStyle = RNSStyle;
+type ViewStyle = RNSStyle;
+type NativeSyntheticEvent<T> = any;
+type NativeTouchEvent = any;
+
+let ENABLE_SCREENS: boolean = true;
+
+export function enableScreens(shouldEnableScreens = true): void {
+  ENABLE_SCREENS = shouldEnableScreens;
+}
+
+export function screensEnabled(): boolean {
+  return ENABLE_SCREENS;
+}
+
+export type StackPresentationTypes =
+| 'push'
+| 'modal'
+| 'transparentModal'
+| 'fullScreenModal'
+| 'formSheet';
+export type StackAnimationTypes = 'default' | 'fade' | 'flip' | 'none';
+
+export interface ScreenProps extends FlexboxLayoutAttributes {
+  active?: 0 | 1;
+  onComponentRef?: (view: any) => void;
+  children?: React.ReactNode;
+  /**
+   *@description A callback that gets called when the current screen appears.
+   */
+  onAppear?: (e: NativeSyntheticEvent<NativeTouchEvent>) => void;
+  /**
+   *@description A callback that gets called when the current screen is dismissed by hardware back (on Android) or dismiss gesture (swipe back or down). The callback takes no arguments.
+   */
+  onDismissed?: (e: NativeSyntheticEvent<NativeTouchEvent>) => void;
+  /**
+   * @type "push" – the new screen will be pushed onto a stack which on iOS means that the default animation will be slide from the side, the animation on Android may vary depending on the OS version and theme.
+   * @type "modal" – the new screen will be presented modally. In addition this allow for a nested stack to be rendered inside such screens
+   * @type "transparentModal" – the new screen will be presented modally but in addition the second to last screen will remain attached to the stack container such that if the top screen is non opaque the content below can still be seen. If "modal" is used instead the below screen will get unmounted as soon as the transition ends.
+   */
+  stackPresentation: StackPresentationTypes;
+  /**
+   *@description Allows for the customization of how the given screen should appear/dissapear when pushed or popped at the top of the stack. The followin values are currently supported:
+   *  @type "default" – uses a platform default animation
+   *  @type "fade" – fades screen in or out
+   *  @type "flip" – flips the screen, requires stackPresentation: "modal" (iOS only)
+   *  @type "none" – the screen appears/dissapears without an animation
+   */
+  stackAnimation?: StackAnimationTypes;
+  /**
+   * @description When set to false the back swipe gesture will be disabled when the parent Screen is on top of the stack. The default value is true.
+   */
+  gestureEnabled?: boolean;
+  style?: RNSStyle;
+}
+
+export type ScreenContainerProps = FlexboxLayoutAttributes;
+
+export interface ScreenStackProps extends FlexboxLayoutAttributes {
+  transitioning?: number;
+  progress?: number;
+  /**
+   * @description A callback that gets called when the current screen finishes its transition.
+   */
+  onFinishTransitioning?: (e: NativeSyntheticEvent<NativeTouchEvent>) => void;
+}
+
+export interface ScreenStackHeaderConfigProps extends FlexboxLayoutAttributes {
+  /**
+   *@description String that representing screen title that will get rendered in the middle section of the header. On iOS the title is centered on the header while on Android it is aligned to the left and placed next to back button (if one is present).
+   */
+  title?: string;
+  /**
+   *@description When set to true the header will be hidden while the parent Screen is on the top of the stack. The default value is false.
+   */
+  hidden?: boolean;
+  /**
+   *@description Controls the color of items rendered on the header. This includes back icon, back text (iOS only) and title text. If you want the title to have different color use titleColor property.
+   */
+  color?: string;
+  /**
+   *@description Customize font family to be used for the title.
+   */
+  titleFontFamily?: string;
+  /**
+   *@description Customize the size of the font to be used for the title.
+   */
+  titleFontSize?: number;
+  /**
+   *@description Allows for setting text color of the title.
+   */
+  titleColor?: string;
+  /**
+   *@description Controls the color of the navigation header.
+   */
+  backgroundColor?: string;
+  /**
+   * @description Boolean that allows for disabling drop shadow under navigation header. The default value is true.
+   */
+  hideShadow?: boolean;
+  /**
+   * @description If set to true the back button will not be rendered as a part of navigation header.
+   */
+  hideBackButton?: boolean;
+  /**
+   * @description Whether to show the back button with a custom left side of the header.
+   */
+  backButtonInCustomView?: boolean;
+  /**
+   * @host (iOS only)
+   * @description When set to true, it makes native navigation bar on iOS semi transparent with blur effect. It is a common way of presenting navigation bar introduced in iOS 11. The default value is false
+   */
+  translucent?: boolean;
+  /**
+   * @host (iOS only)
+   * @description Allows for controlling the string to be rendered next to back button. By default iOS uses the title of the previous screen.
+   */
+  backTitle?: string;
+  /**
+   * @host (iOS only)
+   * @description Allows for customizing font family to be used for back button title on iOS.
+   */
+  backTitleFontFamily?: string;
+  /**
+   * @host (iOS only)
+   * @description Allows for customizing font size to be used for back button title on iOS.
+   */
+  backTitleFontSize?: number;
+  /**
+   * @host (iOS only)
+   * @description When set to true it makes the title display using the large title effect.
+   */
+  largeTitle?: boolean;
+  /**
+   * @host (iOS only)
+   * @description Customize font family to be used for the large title.
+   */
+  largeTitleFontFamily?: string;
+  /**
+   * @host (iOS only)
+   * @description Customize the size of the font to be used for the large title.
+   */
+  largeTitleFontSize?: number;
+  /**
+   *@description Controls the color of the navigation header when the edge of any scrollable content reaches the matching edge of the navigation bar.
+   */
+  largeTitleBackgroundColor?: string;
+  /**
+   * @description Boolean that allows for disabling drop shadow under navigation header when the edge of any scrollable content reaches the matching edge of the navigation bar.
+   */
+  largeTitleHideShadow?: boolean;
+  /**
+   * @host (iOS only)
+   * @description Customize the color to be used for the large title. By default uses the titleColor property.
+   */
+  largeTitleColor?: string;
+  /**
+   * Pass HeaderLeft, HeaderRight and HeaderTitle
+   */
+  children?: React.ReactNode;
+}
+
+
+/**
+ * @see https://github.com/software-mansion/react-native-screens/blob/master/src/screens.web.js
+ */
+export class NativeScreen extends React.Component<ScreenProps> {
+  render() {
+    const { active, style, ...rest } = this.props;
+
+    return (
+      <flexboxLayout
+        style={{
+          ...style,
+          ...(
+            ENABLE_SCREENS && !active ? 
+              {
+                visibility: 'collapse' // Because `display: 'none'` doesn't exist in NativeScript
+              } : 
+              {}
+          ),
+          ...rest
+        }}
+      />
+    );
+  }
+}
+
+export const Screen = NativeScreen;
+
+export const ScreenContainer: React.ElementType<JSX.IntrinsicElements["flexboxLayout"]> = "flexboxLayout";
+
+export const NativeScreenContainer: React.ElementType<JSX.IntrinsicElements["flexboxLayout"]> = "flexboxLayout";
