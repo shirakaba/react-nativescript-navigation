69183c77c517bd2d3b6a6cb3c9ae1c56bada908f
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-10-14 21:22:01 +0100

    feat: authFlowTest

--- sample/app/testComponents/AppContainer.tsx
+++ sample/app/testComponents/AppContainer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react"; 
 import { BaseNavigationContainer } from '@react-navigation/core';
 // import { default as stackNavigatorFactory } from "react-nativescript-navigation/dist/stack/stackNavigatorFactory";
 import { stackNavigatorFactory, tabNavigatorFactory } from "react-nativescript-navigation";
@@ -8,16 +9,16 @@ const StackNavigator = stackNavigatorFactory();
 
 // WARNING: React Hot Loader is incompatible with React Navigation, as it wraps our Screens in a ProxyFacade!
 
-// const AppContainer = () => (
-//     <BaseNavigationContainer>
-//         <TabNavigator.Navigator initialRouteName="first">
-//             <TabNavigator.Screen name="first" component={First}/>
-//             <TabNavigator.Screen name="second" component={Second}/>
-//         </TabNavigator.Navigator>
-//     </BaseNavigationContainer>
-// );
+const tabNavigatorTest = () => (
+    <BaseNavigationContainer>
+        <TabNavigator.Navigator initialRouteName="first">
+            <TabNavigator.Screen name="first" component={First}/>
+            <TabNavigator.Screen name="second" component={Second}/>
+        </TabNavigator.Navigator>
+    </BaseNavigationContainer>
+);
 
-const AppContainer = () => (
+const stackNavigatorTest = () => (
     <BaseNavigationContainer>
         <StackNavigator.Navigator
             initialRouteName="first"
@@ -30,6 +31,107 @@ const AppContainer = () => (
         </StackNavigator.Navigator>
     </BaseNavigationContainer>
 );
+
+const AuthContext = React.createContext<{
+    signIn: (data: any) => Promise<void>;
+    signOut: () => void;
+    signUp: (data: any) => Promise<void>;
+}>(void 0);
+
+interface Action {
+    type: "RESTORE_TOKEN" | "SIGN_IN" | "SIGN_OUT",
+    token?: string,
+}
+interface AuthState {
+    isLoading: boolean,
+    isSignout: boolean,
+    userToken: string|null,
+};
+
+/**
+ * @see https://reactnavigation.org/docs/auth-flow/
+ */
+const authFlowTest = () => {
+    const [state, dispatch] = React.useReducer<(prevState: AuthState, action: Action) => AuthState>(
+        (prevState: AuthState, action: Action) => {
+            switch (action.type) {
+                case 'RESTORE_TOKEN':
+                    return {
+                        ...prevState,
+                        userToken: action.token,
+                        isLoading: false,
+                    };
+                case 'SIGN_IN':
+                    return {
+                        ...prevState,
+                        isSignout: false,
+                        userToken: action.token,
+                    };
+                case 'SIGN_OUT':
+                    return {
+                        ...prevState,
+                        isSignout: true,
+                        userToken: null,
+                    };
+            }
+        },
+        {
+            isLoading: true,
+            isSignout: false,
+            userToken: null,
+        }
+    );
+
+    const authContext = React.useMemo(
+        () => ({
+            signIn: async data => {
+                // In a production app, we need to send some data (usually username, password) to server and get a token
+                // We will also need to handle errors if sign in failed
+                // After getting token, we need to persist the token using `AsyncStorage`
+                // In the example, we'll use a dummy token
+
+                dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            },
+            signOut: () => dispatch({ type: 'SIGN_OUT' }),
+            signUp: async data => {
+                // In a production app, we need to send user data to server and get a token
+                // We will also need to handle errors if sign up failed
+                // After getting token, we need to persist the token using `AsyncStorage`
+                // In the example, we'll use a dummy token
+
+                dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+            },
+        }),
+        []
+    );
+
+    return (
+        <AuthContext.Provider value={authContext}>
+           <BaseNavigationContainer>
+                <StackNavigator.Navigator
+                    initialRouteName="login"
+                    screenOptions={{
+                        headerShown: true,
+                    }}
+                >
+                    {
+                        state.userToken == null ?
+                            (
+                                <>
+                                    <StackNavigator.Screen options={{ headerShown: false }} name="login" component={Login} />
+                                </>
+                            ) :
+                            (
+                                <>
+                                    <StackNavigator.Screen name="home" component={Home} />
+                                </>
+                            )
+                    }
+                </StackNavigator.Navigator>
+            </BaseNavigationContainer>
+        </AuthContext.Provider>
+    );
+}
 
 function First({ navigation }){
     function onButtonTap(){
@@ -76,4 +178,57 @@ function Second({ navigation }){
     );
 }
 
+function Login({ navigation }) {
+    const { signIn } = React.useContext(AuthContext);
+
+    function onButtonTap() {
+        signIn({ username: "dummy", password: "whatever" });
+        // navigation.navigate('home');
+    }
+
+    return (
+        <flexboxLayout
+            style={{
+                flexGrow: 1,
+                width: "100%",
+                height: "100%",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "yellow",
+            }}
+        >
+            <label fontSize={24} text={"Login screen"} />
+            <button onTap={onButtonTap} fontSize={24} text={"Log in"} />
+        </flexboxLayout>
+    );
+}
+
+function Home({ navigation }) {
+    const { signOut } = React.useContext(AuthContext);
+
+    function onButtonTap() {
+        signOut();
+        // navigation.navigate('login');
+    }
+
+    return (
+        <flexboxLayout
+            style={{
+                flexGrow: 1,
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "gold",
+            }}
+        >
+            <label fontSize={24} text={"You're logged in!"} />
+            <button onTap={onButtonTap} fontSize={24} text={"Log out"} />
+        </flexboxLayout>
+    );
+}
+
+// const AppContainer = tabNavigatorTest;
+// const AppContainer = stackNavigatorTest;
+const AppContainer = authFlowTest;
 export default AppContainer;
